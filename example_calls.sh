51a747292e8513e974f9321ce610b9e6084f64b3
David Barragán Merino <david.barragan@kaleidos.net>
2017-07-13 10:51:57 +0200

    Add example call for update

--- example_calls.sh
+++ example_calls.sh
@@ -8,4 +8,15 @@
 # '}'
 
 
-http DELETE localhost:8000/api/projects/project-example-1/mapitems/1d1486e4-4a6b-4222-91fb-4bf63e71b194
+http PUT localhost:8000/api/projects/project-example-1/mapitems/00b3130d-6103-49d5-84da-6347a616de6c <<< '{'\
+'  "name": "caca",'\
+'  "description": "cacota",'\
+'  "type": "point",'\
+'  "data": {'\
+'    "icon": "other",'\
+'    "position": [38.0450, 4.1693]'\
+'  }'\
+'}'
+
+
+# http DELETE localhost:8000/api/projects/project-example-1/mapitems/1d1486e4-4a6b-4222-91fb-4bf63e71b194
