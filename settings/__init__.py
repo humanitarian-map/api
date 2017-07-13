a7263c965f74ed67941dab37548eb496926c5220
andy <andres.gonzalez@kaleidos.net>
2017-07-13 09:07:14 +0200

    Fix __init__

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,6 +4,6 @@ import sys
 try:
     print("Trying import local settings...", file=sys.stderr)
     from .local import *    # noqa
-except ModuleNotFoundError:
+except ImportError:
     print("Import base settings...", file=sys.stderr)
     from .base import *     # noqa
